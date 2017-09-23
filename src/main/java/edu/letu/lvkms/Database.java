39df0e533e2b24f51f2d35c59f11f140dfc1a1b4
cjgriscom <cjgriscom@gmail.com>
2017-09-22 22:11:21 -0400

    Start populating database

--- src/main/java/edu/letu/lvkms/Database.java
+++ src/main/java/edu/letu/lvkms/Database.java
@@ -2,19 +2,31 @@ package edu.letu.lvkms;
 
 import java.io.File;
 import java.util.concurrent.ConcurrentMap;
+import java.util.function.Consumer;
 
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
+import org.mapdb.Serializer;
 
 public class Database {
-	public static void initDB() {
-		
-		DB lvkmsDB = DBMaker.fileDB(new File(getDBDirectory(),"master.db")).make();
-		ConcurrentMap map = lvkmsDB.hashMap("asdasdasd").createOrOpen();
-		map.put("something", "here");
-		
+	DB lvkmsDB;
+
+	public Database() {
+		lvkmsDB = DBMaker
+				.fileDB(new File(getDBDirectory(), "master.db"))
+				.fileMmapEnable()
+				.make();
 	}
 	
+	public void commit(Consumer<DB> consumer) {
+		consumer.accept(lvkmsDB);
+		lvkmsDB.commit();
+	}
+	
+	public void close() {
+		lvkmsDB.close();
+	}
+
 	public static File getDBDirectory() {
 		File dirs = Host.getConfigurationDirectory();
 		if (!dirs.exists()) {
