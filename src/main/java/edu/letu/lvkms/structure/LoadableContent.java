61266897639473c0e1bc79f35324edc653e3b47b
cjgriscom <cjgriscom@gmail.com>
2017-10-22 23:48:31 -0400

    Change contentID to UUID

--- src/main/java/edu/letu/lvkms/structure/LoadableContent.java
+++ src/main/java/edu/letu/lvkms/structure/LoadableContent.java
@@ -1,18 +1,19 @@
 package edu.letu.lvkms.structure;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 public class LoadableContent implements Selectable, Serializable {
 
 	private static final long serialVersionUID = -6743094463689414632L;
 	
-	private final String contentID;
+	private final UUID contentID;
 	
-	public LoadableContent(String contentID) {
+	public LoadableContent(UUID contentID) {
 		this.contentID = contentID;
 	}
 	
-	public String getContentID() {
+	public UUID getContentID() {
 		return contentID;
 	}
 }
