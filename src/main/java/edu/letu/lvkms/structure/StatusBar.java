f51eeaba8566b12ba3d3b8b2e6c284e89c6c7ec3
cjgriscom <cjgriscom@gmail.com>
2017-10-23 04:00:02 -0400

    Populate View and StatusBar

--- src/main/java/edu/letu/lvkms/structure/StatusBar.java
+++ src/main/java/edu/letu/lvkms/structure/StatusBar.java
@@ -1,5 +1,61 @@
 package edu.letu.lvkms.structure;
 
-public class StatusBar {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatusBar implements Serializable {
 	
+	private static final long serialVersionUID = 3700830180680986009L;
+	
+	private final ArrayList<String> stocks;
+	
+	private boolean weather;
+	private boolean time;
+	
+	private String marqueeText = null;
+	
+	public StatusBar() {
+		this.stocks = new ArrayList<>();
+	}
+	
+	public void setWeather(boolean weather) {
+		this.weather = weather;
+	}
+	
+	public boolean hasWeather() {
+		return weather;
+	}
+	
+	public void setTime(boolean time) {
+		this.time = time;
+	}
+	
+	public boolean getTime() {
+		return time;
+	}
+	
+	public boolean hasStocks() {
+		return !stocks.isEmpty();
+	}
+	
+	public List<String> getStocks() {
+		return stocks;
+	}
+	
+	public void setMarquee(String text) {
+		this.marqueeText = text;
+	}
+	
+	public void removeMarquee() {
+		this.marqueeText = null;
+	}
+	
+	public boolean hasMarquee() {
+		return this.marqueeText == null;
+	}
+	
+	public String getMarqueeText() {
+		return marqueeText;
+	}
 }
--- src/main/java/edu/letu/lvkms/structure/View.java
+++ src/main/java/edu/letu/lvkms/structure/View.java
@@ -1,5 +1,47 @@
 package edu.letu.lvkms.structure;
 
-public class View {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class View implements Serializable {
+	
+	private static final long serialVersionUID = 5841199622853631198L;
+	
+	private final UUID viewID;
+	
+	private String name;
+	
+	private final ArrayList<Selectable> buttonBox;
+	
+	private final StatusBar statusBar;
+	
+	public View(String name) {
+		this.name = name;
+		this.viewID = UUID.randomUUID();
+		this.buttonBox = new ArrayList<>();
+		this.statusBar = new StatusBar();
+	}
+	
+	public UUID getViewID() {
+		return viewID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<Selectable> getButtonBox() {
+		return buttonBox;
+	}
+	
+	public StatusBar statusBar() {
+		return statusBar;
+	}
 	
 }
