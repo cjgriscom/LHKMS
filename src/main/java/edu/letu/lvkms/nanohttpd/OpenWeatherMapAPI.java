9bb55d30abf48654a4a76b2e14eb6fc087ebccfb
cjgriscom <cjgriscom@gmail.com>
2017-11-21 18:25:53 -0500

    Let it go

--- src/main/java/edu/letu/lvkms/nanohttpd/OpenWeatherMapAPI.java
+++ src/main/java/edu/letu/lvkms/nanohttpd/OpenWeatherMapAPI.java
@@ -4,10 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.text.DecimalFormat;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -15,30 +11,21 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import org.json.JSONObject;
 
-public class RobinhoodAPI {
-	static final String URL = "https://api.robinhood.com";
-	static final String QUOTES = "/quotes";
+public class OpenWeatherMapAPI {
+	static final String URL = "http://api.openweathermap.org/data/2.5/forecast?q=Longview,US&appid=d2b6ac842005c6f8ed294b31eba32aa5";
 	static HttpClient client = HttpClients.createDefault();
 	
-	public static String getStockTable(String symbol) throws ClientProtocolException, IOException, URISyntaxException {
-		BufferedReader reader = get(symbolQuote(symbol));
+	public static String getWeatherTable() throws ClientProtocolException, IOException, URISyntaxException {
+		BufferedReader reader = get(URL);
 		JSONObject quote = new JSONObject(reader.readLine());
 		reader.close();
 		
-		double price = quote.getDouble("last_trade_price");
-		double prev = quote.getDouble("previous_close");
-		DecimalFormat df = new DecimalFormat();
-		df.setMaximumFractionDigits(2);
+		return quote.toString(3);
 		
-		double changePrice = price-prev;
-		double priceDouble = (price - prev)/prev*100.;
-		boolean up = priceDouble >= 0;
-		String percent = df.format(Math.abs(priceDouble)) + "%";
-		
-		String image = up ? "icons/StockUP.png" : "icons/StockDOWN.png";
+		/*String image = up ? "icons/StockUP.png" : "icons/StockDOWN.png";
 		
 		String table = new String(Files.readAllBytes(Paths.get(
-				RobinhoodAPI.class.getResource("/WebContent/stockTable.html").toURI())))
+				OpenWeatherMapAPI.class.getResource("/WebContent/stockTable.html").toURI())))
 				.replaceAll("\\{SYM\\}", symbol)
 				.replaceAll("\\{IMG\\}", image)
 				.replaceAll("\\{PRICE\\}", df.format(price))
@@ -47,16 +34,13 @@ public class RobinhoodAPI {
 				.replaceAll("\\{RANDOM\\}", ""+(""+Math.random()).hashCode())
 				;
 		
-		return table;
+		return table;*/
 	}
 	public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
 		
-		System.out.println(getStockTable("NVDA"));
+		System.out.println(getWeatherTable());
 	}
 	
-	public static String symbolQuote(String symbol) {
-		return URL + "/quotes/" + symbol + "/";
-	}
 	
 	public static BufferedReader get(String url) throws ClientProtocolException, IOException {
 
