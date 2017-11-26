ddcbe439b4f59fc0aaf2d35831150e381df37d88
cjgriscom <cjgriscom@gmail.com>
2017-11-26 15:18:55 -0500

    Abstract ListView code into common superclass

--- src/main/java/edu/letu/lhkms/javafx/ContentEditor.java
+++ src/main/java/edu/letu/lhkms/javafx/ContentEditor.java
@@ -1,72 +1,33 @@
 package edu.letu.lhkms.javafx;
 
+import java.util.List;
+
 import edu.letu.lhkms.structure.CompleteDatabasePipeline;
 import edu.letu.lhkms.structure.Content;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.HPos;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
+import edu.letu.lhkms.structure.InteractiveList;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import javafx.scene.text.Font;
 
-public class ContentEditor extends ContainerChild {
-	
-	private final ObservableList<ContentItem> contentList = FXCollections.observableArrayList();
-	private final ClientApp app;
-	
-	private class ContentItem extends Label {
-		final Content c;
-		ContentItem(Content c) {
-			super(c.getName());
-			if (!c.isUnused(app.db.get().viewList())) {
-				this.setStyle("-fx-font-weight:bold;");
-			}
-			this.c=c;
-		}
-	}
-	
+public class ContentEditor extends TopLevelListEditor<Content> {
 	public ContentEditor(ClientApp app, Region parent) {
-		super("Content", new SimpleStringProperty("Content Editor"), parent);
-		
-		this.app = app;
-		
-		app.db.addListener((e,o,n) -> repopulate(n));
-		
-		Label conf = new Label("Content Editor");
-		conf.setFont(Font.font(28));
-		ListView<ContentItem> lv = new ListView<>(contentList);
-		ListManager lm = new ListManager(lv, ()->{return app.db.get().contentListModifier();}, () -> {
-			repopulate(app.db.get());
-			app.commitDB();
-		});
-		
-		HBox navigator = new HBox(lv, lm);
-		
-		navigator.translateXProperty().bind(lm.widthProperty().divide(2)); // Center with respect to lv
-		
-		this.setAlignment(Pos.CENTER);
-		int y = 0;
-		this.add(conf, 0, y++);
-		this.add(FXUtil.vspring(), 0, y++);
-		this.add(navigator, 0, y++);
-		this.add(FXUtil.vspring(), 0, y++);
-		for (Node n : getChildren()) 
-			GridPane.setHalignment(n, HPos.CENTER);
-		
-		
+		super("Content", "Content Editor", parent, app);
+	}
+	
+	public Label constructListLabel(Content item) {
+		Label l = new Label(item.getName());
+		if (!item.isUnused(app.db.get().viewList())) {
+			l.setStyle("-fx-font-weight:bold;");
+		}
+		return l;
 	}
 
-	private void repopulate(CompleteDatabasePipeline db) {
-		contentList.clear();
-		db.contentList().forEach((c) -> {
-			this.contentList.add(new ContentItem(c));
-		});
+	@Override
+	public List<Content> getMatchingDatabaseList(CompleteDatabasePipeline db) {
+		return db.contentList();
+	}
+
+	@Override
+	public InteractiveList getMatchingModifierList(CompleteDatabasePipeline db) {
+		return db.contentListModifier();
 	}
 }
---
+++ src/main/java/edu/letu/lhkms/javafx/TopLevelListEditor.java
@@ -0,0 +1,66 @@
+package edu.letu.lhkms.javafx;
+
+import java.util.List;
+
+import edu.letu.lhkms.structure.CompleteDatabasePipeline;
+import edu.letu.lhkms.structure.InteractiveList;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Font;
+
+public abstract class TopLevelListEditor<T> extends ContainerChild {
+	
+	private final ObservableList<Label> labelList = FXCollections.observableArrayList();
+	protected final ClientApp app;
+
+	public TopLevelListEditor(String breadcrumbName, String name, Region parent, ClientApp app) {
+		super(breadcrumbName, new SimpleStringProperty(name), parent);
+		
+		this.app = app;
+		
+		app.db.addListener((e,o,n) -> repopulate(n));
+		
+		Label conf = new Label(name);
+		conf.setFont(Font.font(28));
+		ListView<Label> lv = new ListView<>(labelList);
+		ListManager lm = new ListManager(lv, ()->{return getMatchingModifierList(app.db.get());}, () -> {
+			repopulate(app.db.get());
+			app.commitDB();
+		});
+		
+		HBox navigator = new HBox(lv, lm);
+		
+		navigator.translateXProperty().bind(lm.widthProperty().divide(2)); // Center with respect to lv
+		
+		this.setAlignment(Pos.CENTER);
+		int y = 0;
+		this.add(conf, 0, y++);
+		this.add(FXUtil.vspring(), 0, y++);
+		this.add(navigator, 0, y++);
+		this.add(FXUtil.vspring(), 0, y++);
+		for (Node n : getChildren()) 
+			GridPane.setHalignment(n, HPos.CENTER);
+		
+		
+	}
+
+	private void repopulate(CompleteDatabasePipeline db) {
+		labelList.clear();
+		getMatchingDatabaseList(db).forEach((c) -> {
+			this.labelList.add(constructListLabel(c));
+		});
+	}
+	
+	public abstract List<T> getMatchingDatabaseList(CompleteDatabasePipeline db);
+	public abstract InteractiveList getMatchingModifierList(CompleteDatabasePipeline db);
+	public abstract Label constructListLabel(T item);
+}
